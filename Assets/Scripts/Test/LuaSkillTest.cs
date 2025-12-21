902445e671ef3ee3e53eff7234ac1090d29b8af2
ItsMyGOO <158052974+ItsMyGOO@users.noreply.github.com>
2025-12-21 11:12:54 +0800

    修改skill测试用例

--- Assets/Scripts/Test/LuaSkillTest.cs
+++ Assets/Scripts/Test/LuaSkillTest.cs
@@ -1,4 +1,6 @@
-﻿using Combat.Skill;
+﻿using System.Collections.Generic;
+using Combat.Skill;
+using Core;
 using UnityEngine;
 using XLua;
 
@@ -31,6 +33,49 @@ namespace Game.Test
         void OnDestroy()
         {
             _luaEnv.Dispose();
+        }
+    }
+
+    public class LuaSkillExecutor
+    {
+        private readonly Dictionary<string, LuaTable> _skillCache = new();
+        private LuaEnv _lua;
+
+        public LuaSkillExecutor(LuaEnv lua)
+        {
+            _lua = lua;
+        }
+
+        public void CastSkill(string skillName, ISkillSource caster, ISkillTarget target)
+        {
+            var ctx = new SkillContext
+            {
+                Caster = caster,
+                Target = target,
+                Damage = new SkillContext.DamageContext()
+                {
+                    BaseDamage = caster.Attack,
+                    FinalDamage = caster.Attack
+                }
+            };
+
+            // Lua 技能逻辑
+            GetSkill(skillName).Get<System.Action<SkillContext>>("Cast")(ctx);
+
+            // 统一结算
+            target.TakeDamage(ctx.Damage.FinalDamage);
+        }
+
+        private LuaTable GetSkill(string skillName)
+        {
+            if (_skillCache.TryGetValue(skillName, out var table))
+                return table;
+
+            var require = _lua.Global.Get<System.Func<string, LuaTable>>("require");
+            table = require($"test/{skillName}");
+
+            _skillCache.Add(skillName, table);
+            return table;
         }
     }
 }
