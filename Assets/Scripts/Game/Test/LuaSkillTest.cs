af6df0e4cab70c1e394318783c1996c03cf424cb
ItsMyGOO <158052974+ItsMyGOO@users.noreply.github.com>
2025-12-21 10:40:26 +0800

    重新整理测试场景

---
+++ Assets/Scripts/Game/Test/LuaSkillTest.cs
@@ -0,0 +1,36 @@
+﻿using Combat.Skill;
+using UnityEngine;
+using XLua;
+
+namespace Game.Test
+{
+    public class LuaSkillTest : MonoBehaviour
+    {
+        private LuaEnv _luaEnv;
+
+        void Start()
+        {
+            _luaEnv = new LuaEnv();
+            _luaEnv.AddLoader(CustomLoader);
+
+            // skill test
+            new LuaSkillExecutor(_luaEnv).CastSkill("fireball", new Player(), new Player());
+        }
+
+        private byte[] CustomLoader(ref string filepath)
+        {
+            string fullPath = Application.dataPath + "/Lua/" + filepath + ".lua";
+            if (System.IO.File.Exists(fullPath))
+                return System.Text.Encoding.UTF8.GetBytes(
+                    System.IO.File.ReadAllText(fullPath)
+                );
+
+            return null;
+        }
+
+        void OnDestroy()
+        {
+            _luaEnv.Dispose();
+        }
+    }
+}
--- Assets/Scripts/Game/Test/SkillTest.cs
+++ Assets/Scripts/Game/Test/SkillTest.cs
@@ -2,41 +2,12 @@
 using Combat.Skill;
 using Core;
 using UnityEngine;
-using XLua;
 
 namespace Game.Test
 {
     public class SkillTest : MonoBehaviour
     {
-        private LuaEnv _luaEnv;
-
-        void Start()
-        {
-            _luaEnv = new LuaEnv();
-            _luaEnv.AddLoader(CustomLoader);
-
-            // skill test
-            new LuaSkillExecutor(_luaEnv).CastSkill("fireball", new Player(), new Player());
-            RunSkillTest();
-        }
-
-        private byte[] CustomLoader(ref string filepath)
-        {
-            string fullPath = Application.dataPath + "/Lua/" + filepath + ".lua";
-            if (System.IO.File.Exists(fullPath))
-                return System.Text.Encoding.UTF8.GetBytes(
-                    System.IO.File.ReadAllText(fullPath)
-                );
-
-            return null;
-        }
-
-        void OnDestroy()
-        {
-            _luaEnv.Dispose();
-        }
-
-        private void RunSkillTest()
+        private void Start()
         {
             var attacker = new Player();
             var defender = new Player();
