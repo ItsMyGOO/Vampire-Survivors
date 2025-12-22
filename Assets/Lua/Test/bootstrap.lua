43d2792ed82a91d7cf0316acf950c74646c9ffb9
ItsMyGOO <158052974+ItsMyGOO@users.noreply.github.com>
2025-12-22 15:19:23 +0800

    改名

--- Assets/Lua/Test/bootstrap.lua
+++ Assets/Lua/Test/bootstrap.lua
@@ -9,15 +9,15 @@ package.path = package.path .. ";Assets/Lua/?.lua;Assets/Lua/?/init.lua"
 
 print("=== lua skill pipeline demo start ===")
 
-local fireball = require('Game.Skill.fireball')
-local buff_system = require('Game.Buff.buff_system')
-local fire_boost = require('Game.Buff.Impl.fire_boost')
-
-local caster = { name = "hero", mp = 100 }
-local enemy = { name = "slime", hp = 300 }
-
-buff_system:add(fire_boost.new())
-
-fireball.cast(caster, enemy)
-
-print("enemy hp:", enemy.hp)
+--local fireball = require('Game.Skill.fireball')
+--local buff_system = require('Game.Buff.buff_system')
+--local fire_boost = require('Game.Buff.Impl.fire_boost')
+--
+--local caster = { name = "hero", mp = 100 }
+--local enemy = { name = "slime", hp = 300 }
+--
+--buff_system:add(fire_boost.new())
+--
+--fireball.cast(caster, enemy)
+--
+--print("enemy hp:", enemy.hp)
