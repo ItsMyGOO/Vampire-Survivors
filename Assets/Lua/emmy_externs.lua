839a517eb9055fdad7274bb12a477c5f21ed19d4
ItsMyGOO <158052974+ItsMyGOO@users.noreply.github.com>
2025-12-23 12:34:31 +0800

    添加定义

--- Assets/Lua/emmy_externs.lua
+++ Assets/Lua/emmy_externs.lua
@@ -28,15 +28,23 @@ CS.UnityEngine.Vector3 = {}
 ---@class CS.UnityEngine.Object
 CS.UnityEngine.Object = {}
 
----@class CS.UnityEngine.GameObject
+
+--- GameObject 类型定义扩展
+---@class CS.UnityEngine.GameObject : CS.UnityEngine.Object
+---@field public transform CS.UnityEngine.Transform
+---@field public activeSelf boolean
+---@field public activeInHierarchy boolean
+---@field public name string
+---@field public tag string
+---@field public layer integer
 CS.UnityEngine.GameObject = {}
+
 ---@overload fun(original: CS.UnityEngine.GameObject): CS.UnityEngine.GameObject
 ---@overload fun(original: CS.UnityEngine.GameObject, parent: CS.UnityEngine.Transform): CS.UnityEngine.GameObject
 ---@overload fun(original: CS.UnityEngine.GameObject, position: CS.UnityEngine.Vector3, rotation: CS.UnityEngine.Quaternion): CS.UnityEngine.GameObject
 ---@overload fun(original: CS.UnityEngine.GameObject, position: CS.UnityEngine.Vector3, rotation: CS.UnityEngine.Quaternion, parent: CS.UnityEngine.Transform): CS.UnityEngine.GameObject
 ---@return CS.UnityEngine.GameObject
 function CS.UnityEngine.GameObject.Instantiate(original, param1, param2, param3) end
-
 
 ---@class CS.UnityEngine.Component : CS.UnityEngine.Object
 CS.UnityEngine.Component = {}
