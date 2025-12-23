3a6ac2124eba5b22ed3be5d2fb820cbb5e04db5a
ItsMyGOO <158052974+ItsMyGOO@users.noreply.github.com>
2025-12-23 19:22:54 +0800

    整理一下代码

--- Assets/Scripts/Test/BattleTest.cs
+++ Assets/Scripts/Test/BattleTest.cs
@@ -9,23 +9,6 @@ namespace Test
     {
         void Start()
         {
-            var buffSystem = LuaMain.Env.DoString(@"
-            local bs = BuffSystem.new()
-            bs:add(FireBoost.new())
-            return bs
-        ")[0] as LuaTable;
-        
-            var fireball = LuaMain.Env.Global.Get<LuaTable>("FireBall");
-        
-            var cast = fireball.Get<Action<LuaTable>>("cast");
-        
-            var ctx = LuaMain.Env.NewTable();
-            ctx.Set("caster", new { id = 1 });
-            ctx.Set("target", new { id = 2 });
-            ctx.Set("base_damage", 100);
-            ctx.Set("buff_system", buffSystem);
-        
-            cast(ctx);
         }
     }
 }
